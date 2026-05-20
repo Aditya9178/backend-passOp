2cabf780c5e788487cd79403751f7b833a53c189
Aditya parida <adityaparida917@gmail.com>
2026-05-20 15:21:37 +0530

    url updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyparser.json())
 // app.use(cors())
 
 app.use(cors({
-  origin: "https://frontend-pass-op.vercel.app/"
+  origin: "https://frontend-pass-op.vercel.app"
 }))
 
 // Connection URL
